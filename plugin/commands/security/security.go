93971d01e502f8935118c339e717f35bbf0a9c1b
Christopher Gallo <chrisagallo@gmail.com>
2021-09-09 16:19:47 -0500

    refactored security commands to include paths to ssl/sshkey, added a bunch of unit tests as well

---
+++ plugin/commands/security/security.go
@@ -0,0 +1,83 @@
+package security
+
+import (
+	"github.com/softlayer/softlayer-go/session"
+	"github.com/urfave/cli"
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/bluemix/terminal"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/managers"
+	. "github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
+)
+
+// Make sure to add new commands to security_test.go as well.
+func GetCommandActionBindings(ui terminal.UI, session *session.Session) map[string]func(c *cli.Context) error {
+	securityManager := managers.NewSecurityManager(session)
+
+	CommandActionBindings := map[string]func(c *cli.Context) error{
+
+		//security -  ssh keys
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSHKEY_ADD_NAME: func(c *cli.Context) error {
+			return NewKeyAddCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSHKEY_EDIT_NAME: func(c *cli.Context) error {
+			return NewKeyEditCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSHKEY_LIST_NAME: func(c *cli.Context) error {
+			return NewKeyListCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSHKEY_PRINT_NAME: func(c *cli.Context) error {
+			return NewKeyPrintCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSHKEY_REMOVE_NAME: func(c *cli.Context) error {
+			return NewKeyRemoveCommand(ui, securityManager).Run(c)
+		},
+		// security - ssl certs
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSLCERT_ADD_NAME: func(c *cli.Context) error {
+			return NewCertAddCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSLCERT_EDIT_NAME: func(c *cli.Context) error {
+			return NewCertEditCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSLCERT_DOWNLOAD_NAME: func(c *cli.Context) error {
+			return NewCertDownloadCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSLCERT_REMOVE_NAME: func(c *cli.Context) error {
+			return NewCertRemoveCommand(ui, securityManager).Run(c)
+		},
+		NS_SECURITY_NAME + "-" + CMD_SECURITY_SSLCERT_LIST_NAME: func(c *cli.Context) error {
+			return NewCertListCommand(ui, securityManager).Run(c)
+		},
+		// Aliases to match slcli
+		"ssl-add": func(c *cli.Context) error {
+			return NewCertAddCommand(ui, securityManager).Run(c)
+		},
+		"ssl-edit": func(c *cli.Context) error {
+			return NewCertEditCommand(ui, securityManager).Run(c)
+		},
+		"ssl-download": func(c *cli.Context) error {
+			return NewCertDownloadCommand(ui, securityManager).Run(c)
+		},
+		"ssl-remove": func(c *cli.Context) error {
+			return NewCertRemoveCommand(ui, securityManager).Run(c)
+		},
+		"ssl-list": func(c *cli.Context) error {
+			return NewCertListCommand(ui, securityManager).Run(c)
+		},
+		"sshkey-add": func(c *cli.Context) error {
+			return NewKeyAddCommand(ui, securityManager).Run(c)
+		},
+		"sshkey-edit": func(c *cli.Context) error {
+			return NewKeyEditCommand(ui, securityManager).Run(c)
+		},
+		"sshkey-list": func(c *cli.Context) error {
+			return NewKeyListCommand(ui, securityManager).Run(c)
+		},
+		"sshkey-print": func(c *cli.Context) error {
+			return NewKeyPrintCommand(ui, securityManager).Run(c)
+		},
+		"sshkey-remove": func(c *cli.Context) error {
+			return NewKeyRemoveCommand(ui, securityManager).Run(c)
+		},
+	}
+
+	return CommandActionBindings
+}
--- plugin/commands/security/security_test.go
+++ plugin/commands/security/security_test.go
@@ -5,9 +5,82 @@ import (
 	. "github.com/onsi/gomega"
 
 	"testing"
+	"reflect"
+
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/security"
 )
 
 func TestManagers(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Security Suite")
 }
+
+// These are all the commands in security.go
+var availableCommands = []string{
+	"ssl-add",
+	"ssl-edit",
+	"ssl-list",
+	"ssl-remove",
+	"security-sshkey-list",
+	"sshkey-add",
+	"sshkey-print",
+	"security-cert-download",
+	"security-cert-remove",
+	"ssl-download",
+	"security-sshkey-add",
+	"security-sshkey-print",
+	"sshkey-edit",
+	"sshkey-remove",
+	"sshkey-list",
+	"security-cert-add",
+	"security-cert-edit",
+	"security-cert-list",
+	"security-sshkey-edit",
+	"security-sshkey-remove",
+}
+
+// This test suite exists to make sure commands don't get accidently removed from the actionBindings
+var _ = Describe("Test security.GetCommandActionBindings()", func() {
+	fakeUI := terminal.NewFakeUI()
+	fakeSession := testhelpers.NewFakeSoftlayerSession(nil)
+	commands := security.GetCommandActionBindings(fakeUI, fakeSession)
+	Context("Test Actions", func() {
+		for _, cmdName := range availableCommands {
+			//necessary to ensure the correct value is passed to the closure
+			cmdName := cmdName
+			It("ibmcloud sl "+cmdName, func() {
+				command, exists := commands[cmdName]
+				Expect(exists).To(BeTrue(), cmdName + " not found")
+				// Checks to make sure we actually have a function here.
+				// Test the actual function works in the specific commands test file.
+				Expect(reflect.ValueOf(command).Kind().String()).To(Equal("func"))
+				context := testhelpers.GetCliContext(cmdName)
+				err := command(context)
+				// some commands work without arguments
+				if err == nil {
+					Expect(err).NotTo(HaveOccurred())
+				} else {
+					Expect(err).To(HaveOccurred())
+				}
+			})
+		}
+	})
+	Context("New commands testable", func() {
+		for cmdName, _ := range commands {
+			//necessary to ensure the correct value is passed to the closure
+			cmdName := cmdName
+			It("availableCommands[" +cmdName + "]", func() {
+				found := false
+				for _, value := range availableCommands {
+					if value == cmdName {
+						found = true
+						break
+					}
+				}
+				Expect(found).To(BeTrue(), cmdName + " needs to be added to availableCommands[] in securty_test.go")			
+			})
+		}
+	})
+})
