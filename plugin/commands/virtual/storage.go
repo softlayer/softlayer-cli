5af1d3074e2c8a28d24de50a6bfc0a71c33d9011
Fernando <fernando.ojeda@jalasoft.com>
2021-09-23 09:53:11 -0400

    Add the storage option for the ibmcloud sl vs

---
+++ plugin/commands/virtual/storage.go
@@ -0,0 +1,147 @@
+package virtual
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/bluemix/terminal"
+	"github.com/softlayer/softlayer-go/datatypes"
+	"github.com/urfave/cli"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/errors"
+	slErr "github.ibm.com/SoftLayer/softlayer-cli/plugin/errors"
+	. "github.ibm.com/SoftLayer/softlayer-cli/plugin/i18n"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/managers"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/utils"
+)
+
+type StorageCommand struct {
+	UI                   terminal.UI
+	VirtualServerManager managers.VirtualServerManager
+}
+
+func NewStorageCommand(ui terminal.UI, virtualServerManager managers.VirtualServerManager) (cmd *StorageCommand) {
+	return &StorageCommand{
+		UI:                   ui,
+		VirtualServerManager: virtualServerManager,
+	}
+}
+
+func (cmd *StorageCommand) Run(c *cli.Context) error {
+	if c.NArg() != 1 {
+		return errors.NewInvalidUsageError(T("This command requires one argument."))
+	}
+	vsID, err := strconv.Atoi(c.Args()[0])
+	if err != nil {
+		return slErr.NewInvalidSoftlayerIdInputError("Virtual server ID")
+	}
+
+	outputFormat, err := metadata.CheckOutputFormat(c, cmd.UI)
+	if err != nil {
+		return err
+	}
+
+	iscsiStorageData, err := cmd.VirtualServerManager.GetStorageDetails(vsID, "ISCSI")
+	if err != nil {
+		return cli.NewExitError(T("Failed to get iscsi storage detail for the virtual server {{.ID}}.\n", map[string]interface{}{"ID": vsID})+err.Error(), 2)
+	}
+
+	nasStorageData, err := cmd.VirtualServerManager.GetStorageDetails(vsID, "NAS")
+	if err != nil {
+		return cli.NewExitError(T("Failed to get nas storage detail for the virtual server {{.ID}}.\n", map[string]interface{}{"ID": vsID})+err.Error(), 2)
+	}
+
+	storageCredentials, err := cmd.VirtualServerManager.GetStorageCredentials(vsID)
+	if err != nil {
+		return cli.NewExitError(T("Failed to get the storage credential detail for the virtual server {{.ID}}.\n", map[string]interface{}{"ID": vsID})+err.Error(), 2)
+	}
+
+	portableStorage, err := cmd.VirtualServerManager.GetPortableStorage(vsID)
+	if err != nil {
+		return cli.NewExitError(T("Failed to get the portable storage detail for the virtual server {{.ID}}.\n", map[string]interface{}{"ID": vsID})+err.Error(), 2)
+	}
+
+	localDisks, err := cmd.VirtualServerManager.GetLocalDisks(vsID)
+	if err != nil {
+		return cli.NewExitError(T("Failed to get the local disks detail for the virtual server {{.ID}}.\n", map[string]interface{}{"ID": vsID})+err.Error(), 2)
+	}
+
+	var storageDetailList []interface{}
+	storageDetailList = append(storageDetailList, storageCredentials)
+	storageDetailList = append(storageDetailList, iscsiStorageData)
+	storageDetailList = append(storageDetailList, nasStorageData)
+	storageDetailList = append(storageDetailList, portableStorage)
+	storageDetailList = append(storageDetailList, localDisks)
+
+	if outputFormat == "JSON" {
+		return utils.PrintPrettyJSONList(cmd.UI, storageDetailList)
+	}
+
+	cmd.UI.Print("Block Storage Details\niSCSI")
+	tableCredentials := cmd.UI.Table([]string{T("Username"), T("Password"), T("IQN")})
+	if storageCredentials.Credential != nil && storageCredentials.Credential.Password != nil {
+		tableCredentials.Add(
+			*storageCredentials.Credential.Username,
+			*storageCredentials.Credential.Password,
+			*storageCredentials.Name)
+	}
+	tableCredentials.Print()
+
+	tableIscsi := cmd.UI.Table([]string{T("\nLUN name"), T("capacity"), T("Target address"), T("Location"), T("Notes")})
+	for _, iscsi := range iscsiStorageData {
+		tableIscsi.Add(
+			*iscsi.Username,
+			utils.FormatIntPointer(iscsi.CapacityGb),
+			*iscsi.ServiceResourceBackendIpAddress,
+			*iscsi.AllowedVirtualGuests[0].Datacenter.LongName,
+			*iscsi.Notes)
+	}
+	tableIscsi.Print()
+
+	cmd.UI.Print("\nPortable Storage")
+	tablePortableStorage := cmd.UI.Table([]string{T("Description"), T("Capacity"), T("Location")})
+	for _, portable := range portableStorage {
+		tablePortableStorage.Add(
+			*portable.Description,
+			fmt.Sprintf("%d", portable.Capacity),
+			*portable.BillingItem.Location.LongName)
+	}
+	tablePortableStorage.Print()
+
+	cmd.UI.Print("\nFile Storage Details")
+	tableNas := cmd.UI.Table([]string{T("Volume name"), T("capacity"), T("Host name"), T("Location"), T("Notes")})
+	for _, nas := range nasStorageData {
+		tableNas.Add(
+			*nas.Username,
+			utils.FormatIntPointer(nas.CapacityGb),
+			*nas.ServiceResourceBackendIpAddress,
+			*nas.AllowedVirtualGuests[0].Datacenter.LongName,
+			*nas.Notes)
+	}
+	tableNas.Print()
+
+	cmd.UI.Print("\nSystem storage details")
+	tableLocalDisks := cmd.UI.Table([]string{T("Type"), T("Name"), T("Drive"), T("Capacity")})
+	for _, disk := range localDisks {
+		if disk.DiskImage != nil {
+			capacity := fmt.Sprintf("%d %s", *disk.DiskImage.Capacity, *disk.DiskImage.Units)
+
+			tableLocalDisks.Add(cmd.getLocalType(disk), *disk.MountType, *disk.Device, capacity)
+		}
+	}
+	tableLocalDisks.Print()
+
+	return nil
+}
+
+//Returns the virtual server local disk type.
+//param disks: virtual server local disks.
+func (cmd *StorageCommand) getLocalType(disk datatypes.Virtual_Guest_Block_Device) string {
+	diskType := "System"
+	swapType := disk.DiskImage.Description
+	if strings.Contains(*swapType, "SWAP") {
+		diskType = "Swap"
+	}
+	return diskType
+}
---
+++ plugin/commands/virtual/storage_test.go
@@ -0,0 +1,199 @@
+package virtual_test
+
+import (
+	"errors"
+
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/softlayer/softlayer-go/datatypes"
+	"github.com/softlayer/softlayer-go/sl"
+	"github.com/urfave/cli"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/virtual"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
+)
+
+var _ = Describe("virtual storage", func() {
+	var (
+		fakeUI        *terminal.FakeUI
+		fakeVSManager *testhelpers.FakeVirtualServerManager
+		cmd           *virtual.StorageCommand
+		cliCommand    cli.Command
+	)
+	BeforeEach(func() {
+		fakeUI = terminal.NewFakeUI()
+		fakeVSManager = new(testhelpers.FakeVirtualServerManager)
+		cmd = virtual.NewStorageCommand(fakeUI, fakeVSManager)
+		cliCommand = cli.Command{
+			Name:        metadata.VSStorageMetaData().Name,
+			Description: metadata.VSStorageMetaData().Description,
+			Usage:       metadata.VSStorageMetaData().Usage,
+			Flags:       metadata.VSStorageMetaData().Flags,
+			Action:      cmd.Run,
+		}
+	})
+
+	Describe("virtual storage", func() {
+		Context("virtual storage without id", func() {
+			It("return error", func() {
+				err := testhelpers.RunCommand(cliCommand)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: This command requires one argument."))
+			})
+		})
+
+		Context("virtual storage with wrong id", func() {
+			It("return error", func() {
+				err := testhelpers.RunCommand(cliCommand, "abcd")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Invalid input for 'Virtual server ID'. It must be a positive integer."))
+			})
+		})
+
+		Context("virtual storage ISCSI with server fails", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetStorageDetailsReturns([]datatypes.Network_Storage{}, errors.New("Internal server error"))
+			})
+			It("return error", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Failed to get iscsi storage detail for the virtual server 1234."))
+				Expect(err.Error()).To(ContainSubstring("Internal server error"))
+			})
+		})
+
+		Context("virtual storage credentials with server fails", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetStorageCredentialsReturns(datatypes.Network_Storage_Allowed_Host{}, errors.New("Internal server error"))
+			})
+			It("return error", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Failed to get the storage credential detail for the virtual server 1234."))
+				Expect(err.Error()).To(ContainSubstring("Internal server error"))
+			})
+		})
+
+		Context("virtual portable storage with server fails", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetPortableStorageReturns([]datatypes.Virtual_Disk_Image{}, errors.New("Internal server error"))
+			})
+			It("return error", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Failed to get the portable storage detail for the virtual server 1234."))
+				Expect(err.Error()).To(ContainSubstring("Internal server error"))
+			})
+		})
+
+		Context("virtual local disks with server fails", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetLocalDisksReturns([]datatypes.Virtual_Guest_Block_Device{}, errors.New("Internal server error"))
+			})
+			It("return error", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Failed to get the local disks detail for the virtual server 1234."))
+				Expect(err.Error()).To(ContainSubstring("Internal server error"))
+			})
+		})
+
+		Context("hardware iscsi", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetStorageDetailsReturns([]datatypes.Network_Storage{
+					datatypes.Network_Storage{
+						Id:                              sl.Int(123458),
+						Username:                        sl.String("SL02SEL1234567-20"),
+						CapacityGb:                      sl.Int(16000),
+						ServiceResourceBackendIpAddress: sl.String("10.10.10.10"),
+						AllowedHardware: []datatypes.Hardware{
+							datatypes.Hardware{
+								Datacenter: &datatypes.Location{
+									LongName: sl.String("Dallas 10"),
+								},
+							},
+						},
+						Notes: sl.String("Test notes"),
+					},
+				}, nil)
+			})
+			It("return table", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring("SL02SEL1234567-20"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("16000"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("10.10.10.10"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Dallas 10"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Test notes"))
+			})
+		})
+
+		Context("hardware Credentials", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetStorageCredentialsReturns(datatypes.Network_Storage_Allowed_Host{
+					Credential: &datatypes.Network_Storage_Credential{
+						Username: sl.String("SL02SU1234567-H1"),
+						Password: sl.String("fMnY59hjkhkj"),
+					},
+					Name: sl.String("iqn.2021-07.com.ibm:SL02SU1234567-h1"),
+				}, nil)
+			})
+			It("return table", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring("SL02SU1234567-H1"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("fMnY59hjkhkj"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("iqn.2021-07.com.ibm:SL02SU1234567-h1"))
+			})
+		})
+
+		Context("hardware portable storage", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetPortableStorageReturns([]datatypes.Virtual_Disk_Image{
+					datatypes.Virtual_Disk_Image{
+						Description: sl.String("Test Description"),
+						Capacity:    sl.Int(16000),
+						BillingItem: &datatypes.Billing_Item_Virtual_Disk_Image{
+							Billing_Item: datatypes.Billing_Item{
+								Location: &datatypes.Location{
+									LongName: sl.String("Dallas 10"),
+								},
+							},
+						},
+					},
+				}, nil)
+			})
+			It("return table", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Test Description"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("16000"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Dallas 10"))
+			})
+		})
+
+		Context("hardware local disks", func() {
+			BeforeEach(func() {
+				fakeVSManager.GetLocalDisksReturns([]datatypes.Virtual_Guest_Block_Device{
+					datatypes.Virtual_Guest_Block_Device{
+						MountType: sl.String("Disk"),
+						Device:    sl.String("1"),
+						DiskImage: &datatypes.Virtual_Disk_Image{
+							Capacity: sl.Int(100),
+							Units:    sl.String("GB"),
+						},
+					},
+				}, nil)
+			})
+			It("return table", func() {
+				err := testhelpers.RunCommand(cliCommand, "1234")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Disk"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("1"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("100"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("GB"))
+			})
+		})
+	})
+})
