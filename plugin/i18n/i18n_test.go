6fdfdb12dfae56b461a9420e85adff4769da3e66
Christopher Gallo <chrisagallo@gmail.com>
2021-09-27 18:03:41 -0500

    #89 fixed ja_JP issues, added tests for i18n to prevent that in the future

---
+++ plugin/i18n/i18n_test.go
@@ -0,0 +1,33 @@
+package i18n_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/bluemix/configuration/core_config"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/i18n"
+)
+
+func TestI18N(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "I18N Suite")
+}
+
+var _ = Describe("I18NTests", func() {
+	coreConfig := core_config.NewCoreConfig(func(error) {})
+	detector := new(testhelpers.FakeDetector)
+	Describe("Language Init Tests", func() {
+		Context("Tests All Languages", func() {
+			for _, language := range i18n.SUPPORTED_LOCALES {
+				language := language
+				It("Testing " + language, func() {
+					detector.DetectLocaleReturns(language)
+					translator := i18n.Init(coreConfig, detector)
+					Expect(translator("Recurring Price")).To(Equal("Recurring Price"))
+				})
+			}
+
+		})
+	})
+})
