3805ffb7c91f8cde0e277b61d021c06039ef55fa
caberos <Daniel.Cabero@ibm.com>
2021-11-02 10:24:15 -0400

    update i18n

--- plugin/i18n/i18n_test.go
+++ plugin/i18n/i18n_test.go
@@ -1,12 +1,12 @@
 package i18n_test
 
 import (
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/bluemix/configuration/core_config"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"testing"
-	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/bluemix/configuration/core_config"
-	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/i18n"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
+	"testing"
 )
 
 func TestI18N(t *testing.T) {
@@ -21,7 +21,7 @@ var _ = Describe("I18NTests", func() {
 		Context("Tests All Languages", func() {
 			for _, language := range i18n.SUPPORTED_LOCALES {
 				language := language
-				It("Testing " + language, func() {
+				It("Testing "+language, func() {
 					detector.DetectLocaleReturns(language)
 					translator := i18n.Init(coreConfig, detector)
 					Expect(translator("Recurring Price")).To(Equal("Recurring Price"))
