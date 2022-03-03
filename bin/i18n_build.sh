87d275a4f25112063096e0c5700cd326e33743cc
Christopher Gallo <chrisagallo@gmail.com>
2022-03-03 13:55:07 -0600

    Removed the requirements for i18n4go, and instead just included the binary for it

--- bin/i18n_build.sh
+++ bin/i18n_build.sh
@@ -11,8 +11,8 @@ DIRECTORIES="plugin/commands/ plugin/metadata/ plugin/version/ plugin/client/ pl
 #     i18n4go -c extract-strings -v -o plugin/i18n/tmp_resources/$i -d $i  -r --ignore-regexp  ".*test.*"
 
 # done
-i18n4go -c extract-strings -v -o plugin/i18n/tmp_resources/ -d plugin  -r --ignore-regexp  ".*test.*" --output-match-package
-i18n4go -c merge-strings -v -r -d plugin/i18n/tmp_resources/
+./bin/i18n4go -c extract-strings -v -o plugin/i18n/tmp_resources/ -d plugin  -r --ignore-regexp  ".*test.*" --output-match-package
+./bin/i18n4go -c merge-strings -v -r -d plugin/i18n/tmp_resources/
 # 
 # i18n4go -c merge-strings -v -d plugin/i18n/tmp_resources/ 
 # mv plugin/i18n/tmp_resources/en.all.json plugin/i18n/resources/en_US.all.json
