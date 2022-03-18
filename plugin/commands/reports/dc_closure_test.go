141b35bfde6919e839cc4f00d39a4189b5fa77cf
Christopher Gallo <chrisagallo@gmail.com>
2022-03-18 16:59:01 -0500

    Finished test

--- plugin/commands/reports/dc_closure_test.go
+++ plugin/commands/reports/dc_closure_test.go
@@ -1,6 +1,7 @@
 package reports_test
 
 import (
+	"fmt"
 	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -18,10 +19,13 @@ var _ = Describe("Reports Datacenter-Closures", func() {
 		cmd         *reports.DCClosuresCommand
 		cliCommand  cli.Command
 		fakeSession *session.Session
+		handler		testhelpers.FakeTransportHandler
 	)
 	BeforeEach(func() {
 		fakeUI = terminal.NewFakeUI()
 		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
+		handler = fakeSession.TransportHandler.(testhelpers.FakeTransportHandler)
+
 		cmd = reports.NewDCClosuresCommand(fakeUI, fakeSession)
 		cliCommand = cli.Command{
 			Name:        reports.DCClosuresMetaData().Name,
@@ -45,7 +49,29 @@ var _ = Describe("Reports Datacenter-Closures", func() {
 				Expect(err).NotTo(HaveOccurred())
 				outputs := fakeUI.Outputs()
 				Expect(outputs).To(ContainSubstring("\"Name\": \"imageTest.ibmtest.com\""))
-
+			})
+		})
+		Context("Error Handling", func() {
+			It("SoftLayer_Search::advancedSearch() Error", func() {
+				handler.AddApiError("SoftLayer_Search", "advancedSearch", 500, "BAD")
+				err := testhelpers.RunCommand(cliCommand, "--output=JSON")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("BAD: BAD (HTTP 500)"))
+			})
+			It("SoftLayer_Network_Pod::getAllObjects() Error", func() {
+				handler.AddApiError("SoftLayer_Network_Pod", "getAllObjects", 500, "ERRRR")
+				fmt.Printf("API ERRORS ARE NOW\n%v", handler.ErrorMap)
+				err := testhelpers.RunCommand(cliCommand, "--output=JSON")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("ERRRR: ERRRR (HTTP 500)"))
+			})
+			It("Outputs NOT JSON", func() {
+				err := testhelpers.RunCommand(cliCommand, "--output=boson")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: Invalid output format, only JSON is supported now."))
+			})
+			AfterEach(func() {
+				handler.ClearErrors()
 			})
 		})
 	})
--- plugin/commands/reports/reports_test.go
+++ plugin/commands/reports/reports_test.go
@@ -1,13 +1,93 @@
 package reports_test
 
 import (
+	"reflect"
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"testing"
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/plugin"
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/reports"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
 )
 
 func TestManagers(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Reports Suite")
 }
+
+
+var availableCommands = []string{
+	"reports-datacenter-closures",
+}
+
+
+
+// This test suite exists to make sure commands don't get accidently removed from the actionBindings
+var _ = Describe("Test reports.GetCommandActionBindings()", func() {
+	var (
+		context plugin.PluginContext
+	)
+	fakeUI := terminal.NewFakeUI()
+	fakeSession := testhelpers.NewFakeSoftlayerSession(nil)
+	context = plugin.InitPluginContext("softlayer")
+	commands := reports.GetCommandActionBindings(context, fakeUI, fakeSession)
+
+	Context("Test Actions", func() {
+		for _, cmdName := range availableCommands {
+			//necessary to ensure the correct value is passed to the closure
+			cmdName := cmdName
+			It("ibmcloud sl "+cmdName, func() {
+				command, exists := commands[cmdName]
+				Expect(exists).To(BeTrue(), cmdName+" not found")
+				// Checks to make sure we actually have a function here.
+				// Test the actual function works in the specific commands test file.
+				Expect(reflect.ValueOf(command).Kind().String()).To(Equal("func"))
+				context := testhelpers.GetCliContext(cmdName)
+				err := command(context)
+				// some commands work without arguments
+				if err == nil {
+					Expect(err).NotTo(HaveOccurred())
+				} else {
+					Expect(err).To(HaveOccurred())
+				}
+			})
+		}
+	})
+
+	Context("New commands testable", func() {
+		for cmdName, _ := range commands {
+			//necessary to ensure the correct value is passed to the closure
+			cmdName := cmdName
+			It("availableCommands["+cmdName+"]", func() {
+				found := false
+				for _, value := range availableCommands {
+					if value == cmdName {
+						found = true
+						break
+					}
+				}
+				Expect(found).To(BeTrue(), cmdName+" needs to be added to availableCommands[] in user.go")
+			})
+		}
+	})
+
+	Context("Report Namespace", func() {
+		It("Report Name Space", func() {
+			Expect(reports.ReportsNamespace().ParentName).To(ContainSubstring("sl"))
+			Expect(reports.ReportsNamespace().Name).To(ContainSubstring("report"))
+			Expect(reports.ReportsNamespace().Description).To(ContainSubstring("Classic Infrastructure Reports"))
+		})
+	})
+
+	Context("User MetaData", func() {
+		It("User MetaData", func() {
+			Expect(reports.ReportsMetaData().Category).To(ContainSubstring("sl"))
+			Expect(reports.ReportsMetaData().Name).To(ContainSubstring("reports"))
+			Expect(reports.ReportsMetaData().Usage).To(ContainSubstring("${COMMAND_NAME} sl reports"))
+			Expect(reports.ReportsMetaData().Description).To(ContainSubstring("Classic Infrastructure Reports"))
+		})
+	})
+})
