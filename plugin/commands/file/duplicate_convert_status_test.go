34fa044eebb41c1d39890d3551ac62afc14aee94
Brian Flores <brian.flores@ibm.com>
2022-06-13 17:17:11 -0400

    added unit tests

---
+++ plugin/commands/file/duplicate_convert_status_test.go
@@ -0,0 +1,89 @@
+package file_test
+
+import (
+	"fmt"
+
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/softlayer/softlayer-go/session"
+
+	"github.com/urfave/cli"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/block"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/file"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
+)
+
+var _ = Describe("file duplicate-convert-status", func() {
+	var (
+		fakeUI      *terminal.FakeUI
+		cmd         *block.DuplicateConvertStatusCommand
+		cliCommand  cli.Command
+		fakeSession *session.Session
+		fakeHandler *testhelpers.FakeTransportHandler
+	)
+	BeforeEach(func() {
+		fakeUI = terminal.NewFakeUI()
+		fakeSession = testhelpers.NewFakeSoftlayerSession(nil)
+		fakeHandler = testhelpers.GetSessionHandler(fakeSession)
+
+		cmd = block.NewDuplicateConvertStatusCommand(fakeUI, fakeSession)
+		cliCommand = cli.Command{
+			Name:        file.FileDuplicateConvertStatusMetaData().Name,
+			Description: file.FileDuplicateConvertStatusMetaData().Description,
+			Usage:       file.FileDuplicateConvertStatusMetaData().Usage,
+			Flags:       file.FileDuplicateConvertStatusMetaData().Flags,
+			Action:      cmd.Run,
+		}
+	})
+	AfterEach(func() {
+		fakeHandler.ClearApiCallLogs()
+		fakeHandler.ClearErrors()
+	})
+	Describe("file duplicate-convert-status", func() {
+
+		Context("Return error", func() {
+			It("Set command without Id", func() {
+				err := testhelpers.RunCommand(cliCommand)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: This command requires one argument."))
+			})
+
+			It("Set command with an invalid Id", func() {
+				err := testhelpers.RunCommand(cliCommand, "abcde")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Invalid input for 'Volume ID'. It must be a positive integer."))
+			})
+
+			It("Set invalid output", func() {
+				err := testhelpers.RunCommand(cliCommand, "123456", "--output=xml")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: Invalid output format, only JSON is supported now."))
+			})
+
+			It("SoftLayer_Network_Storage::getDuplicateConversionStatus() Error", func() {
+				fakeHandler.AddApiError("SoftLayer_Network_Storage", "getDuplicateConversionStatus", 500, "ERRRR")
+				fmt.Printf("API ERRORS ARE NOW\n%v", fakeHandler.ErrorMap)
+				err := testhelpers.RunCommand(cliCommand, "123456")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("ERRRR: ERRRR (HTTP 500)"))
+			})
+
+			AfterEach(func() {
+				fakeHandler.ClearErrors()
+			})
+		})
+
+		Context("Return no error", func() {
+			It("Return no error", func() {
+				err := testhelpers.RunCommand(cliCommand, "123456")
+				Expect(err).NotTo(HaveOccurred())
+				outputs := fakeUI.Outputs()
+				Expect(outputs).To(ContainSubstring("2022-06-13 14:59:17"))
+				Expect(outputs).To(ContainSubstring("68"))
+				Expect(outputs).To(ContainSubstring("SL02SEVC123456_74"))
+			})
+		})
+	})
+})
