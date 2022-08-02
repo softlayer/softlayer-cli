eabd47976d4cdee686383af829331b5b0ef3ba0f
edsonarios <edsonrios9@gmail.com>
2022-08-02 11:38:12 -0400

    Added new command - ibmcloud sl cdn detail

--- plugin/managers/cdn.go
+++ plugin/managers/cdn.go
@@ -1,6 +1,9 @@
 package managers
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/softlayer/softlayer-go/datatypes"
 	"github.com/softlayer/softlayer-go/services"
 	"github.com/softlayer/softlayer-go/session"
@@ -8,6 +11,8 @@ import (
 
 type CdnManager interface {
 	GetNetworkCdnMarketplaceConfigurationMapping(mask string) ([]datatypes.Container_Network_CdnMarketplace_Configuration_Mapping, error)
+	GetDetailCDN(uniqueId int, mask string) (datatypes.Container_Network_CdnMarketplace_Configuration_Mapping, error)
+	GetUsageMetrics(uniqueId int, history int, mask string) (datatypes.Container_Network_CdnMarketplace_Metrics, error)
 }
 
 type cdnManager struct {
@@ -28,4 +33,37 @@ https://sldn.softlayer.com/reference/services/SoftLayer_Network_CdnMarketplace_C
 */
 func (a cdnManager) GetNetworkCdnMarketplaceConfigurationMapping(mask string) ([]datatypes.Container_Network_CdnMarketplace_Configuration_Mapping, error) {
 	return a.CdnService.Mask(mask).ListDomainMappings()
+}
+
+/*
+SOAP API will return the domain mapping based on the uniqueId.
+https://sldn.softlayer.com/reference/services/SoftLayer_Network_CdnMarketplace_Configuration_Mapping/listDomainMappingByUniqueId/
+*/
+func (a cdnManager) GetDetailCDN(uniqueId int, mask string) (datatypes.Container_Network_CdnMarketplace_Configuration_Mapping, error) {
+	cdnId := strconv.Itoa(uniqueId)
+	cdn, err := a.CdnService.Mask(mask).ListDomainMappingByUniqueId(&cdnId)
+	if err != nil {
+		return datatypes.Container_Network_CdnMarketplace_Configuration_Mapping{}, err
+	}
+	return cdn[0], nil
+}
+
+/*
+SOAP API will return the domain mapping based on the uniqueId.
+https://sldn.softlayer.com/reference/services/SoftLayer_Network_CdnMarketplace_Configuration_Mapping/listDomainMappingByUniqueId/
+*/
+func (a cdnManager) GetUsageMetrics(uniqueId int, history int, mask string) (datatypes.Container_Network_CdnMarketplace_Metrics, error) {
+	NetworkCdnMarketplaceMetricsService := services.GetNetworkCdnMarketplaceMetricsService(a.Session)
+
+	startDate := int(time.Now().AddDate(0, 0, (history * -1)).Unix())
+	endDate := int(time.Now().Unix())
+	frequency := "aggregate"
+	cdnId := strconv.Itoa(uniqueId)
+
+	cdn, err := NetworkCdnMarketplaceMetricsService.Mask(mask).GetMappingUsageMetrics(&cdnId, &startDate, &endDate, &frequency)
+	if err != nil {
+		return datatypes.Container_Network_CdnMarketplace_Metrics{}, err
+	}
+
+	return cdn[0], nil
 }
