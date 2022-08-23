07fdadf8613de3bd139d0b206ef586b663a12870
Brian Flores <brian.flores@ibm.com>
2022-08-23 10:53:19 -0400

    Refactorized command

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -298,6 +298,7 @@ func getTopCobraCommand(ui terminal.UI, session *session.Session) *cobra.Command
 	cobraCmd.PersistentFlags().Var(slCommand.OutputFlag, "output", "--output=JSON for json output.")
 	// Commands
 	cobraCmd.AddCommand(callapi.NewCallAPICommand(slCommand))
+	cobraCmd.AddCommand(autoscale.SetupCobraCommands(slCommand))
 	cobraCmd.AddCommand(account.SetupCobraCommands(slCommand))
 	cobraCmd.AddCommand(email.SetupCobraCommands(slCommand))
 	cobraCmd.AddCommand(reports.SetupCobraCommands(slCommand))
