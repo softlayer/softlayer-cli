1bba7096e1428fe41be3e0e24a4fb96c3e195d4d
edsonarios <edsonrios9@gmail.com>
2022-08-31 09:00:15 -0400

    Part2 Refactored subnet: edit, list, lookup, route commands

--- plugin/commands/subnet/route_test.go
+++ plugin/commands/subnet/route_test.go
@@ -7,42 +7,41 @@ import (
 	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/urfave/cli"
+	"github.com/softlayer/softlayer-go/session"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/subnet"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
 )
 
 var _ = Describe("Subnet Route", func() {
 	var (
 		fakeUI             *terminal.FakeUI
+		cliCommand         *subnet.RouteCommand
+		fakeSession        *session.Session
+		slCommand          *metadata.SoftlayerCommand
 		fakeNetworkManager *testhelpers.FakeNetworkManager
-		cmd                *subnet.RouteCommand
-		cliCommand         cli.Command
 	)
 	BeforeEach(func() {
 		fakeUI = terminal.NewFakeUI()
+		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
+		slCommand = metadata.NewSoftlayerCommand(fakeUI, fakeSession)
+		cliCommand = subnet.NewRouteCommand(slCommand)
+		cliCommand.Command.PersistentFlags().Var(cliCommand.OutputFlag, "output", "--output=JSON for json output.")
 		fakeNetworkManager = new(testhelpers.FakeNetworkManager)
-		cmd = subnet.NewRouteCommand(fakeUI, fakeNetworkManager)
-		cliCommand = cli.Command{
-			Name:        subnet.SubnetRouteMetaData().Name,
-			Description: subnet.SubnetRouteMetaData().Description,
-			Usage:       subnet.SubnetRouteMetaData().Usage,
-			Flags:       subnet.SubnetRouteMetaData().Flags,
-			Action:      cmd.Run,
-		}
+		cliCommand.NetworkManager = fakeNetworkManager
 	})
 
 	Describe("Subnet Route", func() {
 		Context("Subnet route without ID", func() {
 			It("return error", func() {
-				err := testhelpers.RunCommand(cliCommand)
+				err := testhelpers.RunCobraCommand(cliCommand.Command)
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: This command requires one argument."))
 			})
 		})
 		Context("Subnet route with wrong subnet ID", func() {
 			It("return error", func() {
-				err := testhelpers.RunCommand(cliCommand, "abc")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "abc")
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Invalid input for 'Subnet ID'. It must be a positive integer."))
 			})
@@ -50,7 +49,7 @@ var _ = Describe("Subnet Route", func() {
 
 		Context("Subnet route without -t", func() {
 			It("return error", func() {
-				err := testhelpers.RunCommand(cliCommand, "-i", "1234", "1234567")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "-i", "1234", "1234567")
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: [-t/--type] is required."))
 			})
@@ -58,7 +57,7 @@ var _ = Describe("Subnet Route", func() {
 
 		Context("Subnet route without -i", func() {
 			It("return error", func() {
-				err := testhelpers.RunCommand(cliCommand, "-t", "SoftLayer_Network_Subnet_IpAddress", "1234567")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "-t", "SoftLayer_Network_Subnet_IpAddress", "1234567")
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: [-i/--type-id] is required."))
 			})
@@ -69,7 +68,7 @@ var _ = Describe("Subnet Route", func() {
 				fakeNetworkManager.RouteReturns(false, errors.New("Internal Server Error"))
 			})
 			It("return error", func() {
-				err := testhelpers.RunCommand(cliCommand, "-i", "1234", "-t", "SoftLayer_Network_Subnet_IpAddress", "1234567")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "-i", "1234", "-t", "SoftLayer_Network_Subnet_IpAddress", "1234567")
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Failed to route using the type: SoftLayer_Network_Subnet_IpAddress and identifier: 1234.\n"))
 				Expect(err.Error()).To(ContainSubstring("Internal Server Error"))
@@ -81,7 +80,7 @@ var _ = Describe("Subnet Route", func() {
 				fakeNetworkManager.RouteReturns(true, nil)
 			})
 			It("return no error", func() {
-				err := testhelpers.RunCommand(cliCommand, "-i", "1234", "-t", "SoftLayer_Network_Subnet_IpAddress", "1234567")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "-i", "1234", "-t", "SoftLayer_Network_Subnet_IpAddress", "1234567")
 				Expect(err).NotTo(HaveOccurred())
 				Expect(fakeUI.Outputs()).To(ContainSubstrings([]string{"OK"}))
 				Expect(fakeUI.Outputs()).To(ContainSubstrings([]string{"The transaction to route is created, routes will be updated in one or two minutes."}))
