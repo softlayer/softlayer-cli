6e42b9587b0f0c44bdedc4228b906a0ba9eb24c7
Brian Flores <brian.flores@ibm.com>
2022-08-31 15:14:59 -0400

    refactor rest of order commands

--- plugin/commands/order/quote_save.go
+++ plugin/commands/order/quote_save.go
@@ -3,8 +3,7 @@ package order
 import (
 	"strconv"
 
-	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/bluemix/terminal"
-	"github.com/urfave/cli"
+	"github.com/spf13/cobra"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/errors"
 	. "github.ibm.com/SoftLayer/softlayer-cli/plugin/i18n"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/managers"
@@ -13,35 +12,41 @@ import (
 )
 
 type QuoteSaveCommand struct {
-	UI           terminal.UI
+	*metadata.SoftlayerCommand
 	OrderManager managers.OrderManager
+	Command      *cobra.Command
 }
 
-func NewQuoteSaveCommand(ui terminal.UI, orderManager managers.OrderManager) (cmd *QuoteSaveCommand) {
-	return &QuoteSaveCommand{
-		UI:           ui,
-		OrderManager: orderManager,
+func NewQuoteSaveCommand(sl *metadata.SoftlayerCommand) (cmd *QuoteSaveCommand) {
+	thisCmd := &QuoteSaveCommand{
+		SoftlayerCommand: sl,
+		OrderManager:     managers.NewOrderManager(sl.Session),
 	}
+
+	cobraCmd := &cobra.Command{
+		Use:   "quote-save " + T("IDENTIFIER"),
+		Short: T("Save a quote"),
+		Args:  metadata.OneArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return thisCmd.Run(args)
+		},
+	}
+
+	thisCmd.Command = cobraCmd
+	return thisCmd
 }
 
-func (cmd *QuoteSaveCommand) Run(c *cli.Context) error {
-	if c.NArg() != 1 {
-		return errors.NewInvalidUsageError(T("This command requires one argument."))
-	}
+func (cmd *QuoteSaveCommand) Run(args []string) error {
+	outputFormat := cmd.GetOutputFlag()
 
-	outputFormat, err := metadata.CheckOutputFormat(c, cmd.UI)
-	if err != nil {
-		return err
-	}
-
-	quoteId, err := strconv.Atoi(c.Args()[0])
+	quoteId, err := strconv.Atoi(args[0])
 	if err != nil {
 		return errors.NewInvalidSoftlayerIdInputError("Quote ID")
 	}
 
 	quote, err := cmd.OrderManager.SaveQuote(quoteId)
 	if err != nil {
-		return cli.NewExitError(T("Failed to save Quote.\n")+err.Error(), 2)
+		return errors.NewAPIError(T("Failed to save Quote.\n"), err.Error(), 2)
 	}
 
 	table := cmd.UI.Table([]string{T("Id"), T("Name"), T("Created"), T("Modified"), T("Status")})
@@ -55,19 +60,4 @@ func (cmd *QuoteSaveCommand) Run(c *cli.Context) error {
 
 	utils.PrintTable(cmd.UI, table, outputFormat)
 	return nil
-}
-
-func OrderQuoteSaveMetaData() cli.Command {
-	return cli.Command{
-		Category:    "order",
-		Name:        "quote-save",
-		Description: T("Save a quote"),
-		Usage: T(`${COMMAND_NAME} sl order quote-save IDENTIFIER [OPTIONS]
-
-EXAMPLE: 
-	${COMMAND_NAME} sl order quote-save 123456`),
-		Flags: []cli.Flag{
-			metadata.OutputFlag(),
-		},
-	}
 }
