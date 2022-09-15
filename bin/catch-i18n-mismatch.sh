326c091a5984a61e2cc60ca4ef1ccf8cd8d6a035
Christopher Gallo <chrisagallo@gmail.com>
2022-09-15 14:14:02 -0500

    Updating catch-18n-mismatch and split_i18n files to be more productive

--- bin/catch-i18n-mismatch.sh
+++ bin/catch-i18n-mismatch.sh
@@ -17,6 +17,7 @@ OUTPUT=""
 ADD_JSON_OUT=$'[\n'
 DEL_JSON_OUT=$'[\n'
 UNTRANSLATED=$'[\n'
+
 if [[ "$RESULTS" =~ ^"OKTotal time: "[0-9\.]*ms$ ]]
 then
     echo $RESULTS
@@ -30,11 +31,15 @@ while IFS= read -r line; do
     OUTPUT="${OUTPUT}${line}"
 
     # These strings need to be ADDED
+
     if [[ $line =~ "exists in the code, but not in en_US" ]]
     then
         
-        JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in the code, but not in en_US/{\"id\": \1, \"translation\": \1},/g"`
-        # echo ">>> |$JSON| <<<"
+        # JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in the code, but not in en_US/{\"id\": \1, \"translation\": \1},/g"`
+        # the \\\\ is because BASH will drop one of the \'s, returning the \" back to ".
+        # This way when bash does that it ends up with \" in the json file
+        OUTPUT=`echo "$OUTPUT" | sed -E "s/\"(.+)\" exists in the code, but not in en_US/\1/g" | sed -E   's/"/\\\\"/g'`
+        JSON="{\"id\": \"$OUTPUT\", \"translation\": \"$OUTPUT\"},"
         OUTPUT=""
         
         # This printf bit is required for bash to put real newlines between missing translations, but preserve
@@ -46,8 +51,8 @@ while IFS= read -r line; do
     # Need to REMOVE these strings
     elif [[ $line =~ "exists in en_US, but not in the code" ]]
     then
-        
-        JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in en_US, but not in the code/{\"id\": \1, \"translation\": \1},/g"`
+        OUTPUT=`echo "$OUTPUT" | sed -E "s/\"(.+)\" exists in en_US, but not in the code/\1/g" | sed -E   's/"/\\\\"/g'`
+        JSON="{\"id\": \"$OUTPUT\", \"translation\": \"$OUTPUT\"},"
         # echo ">>> |$JSON| <<<"
         OUTPUT=""
         
@@ -60,7 +65,9 @@ while IFS= read -r line; do
     elif [[ $line =~ "exists in en_US, but not in "([a-zA-Z_]{5,7})"" ]]
     then
         # This is "normal", means the text has not been translated.
-        JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in en_US, but not in ([a-zA-Z_]{5,7})/{\"id\": \1, \"translation\": \1, \"file\": \2.all.json},/g"`
+        OUTPUT=`echo "$OUTPUT" | sed -E "s/\"(.+)\" exists in en_US, but not in ([a-zA-Z_]{5,7})/\1/g" | sed -E   's/"/\\\\"/g'`
+        JSON="{\"id\": \"$OUTPUT\", \"translation\": \"$OUTPUT\"},"
+        # JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in en_US, but not in ([a-zA-Z_]{5,7})/{\"id\": \1, \"translation\": \1, \"file\": \2.all.json},/g"`
         OUTPUT=""
         UNTRANSLATED=`printf "%s\n    %s" "${UNTRANSLATED}" "${JSON}"`
         JSON=""
@@ -69,11 +76,14 @@ while IFS= read -r line; do
     # A translation exists in some files, but not in en_US
     elif [[ $line =~ "exists in "([a-zA-Z_]{5,7})", but not in en_US" ]]
     then
-        JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in [a-zA-Z_]{5,7}, but not in en_US/{\"id\": \1, \"translation\": \1},/g"`
-        # JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in ([a-zA-Z_]{5,7}), but not in ([a-zA-Z_]{5,7})/id: \1 is missing from \3.all.json, but exists in \2.all.json/g"`
-        printf "\033[0;33m>>>  $JSON <<<\033[0m\n"
+        OUTPUT=`echo "$OUTPUT" | sed -E "s/\"(.+)\" exists in [a-zA-Z_]{5,7}, but not in en_US/\1/g" | sed -E   's/"/\\\\"/g'`
+        JSON="{\"id\": \"$OUTPUT\", \"translation\": \"$OUTPUT\"},"
+        printf "\033[0;33m>>>  |%s| <<<\033[0m\n" "${OUTPUT}"
+        # JSON=`echo "$OUTPUT" | sed -E "s/(.+) exists in [a-zA-Z_]{5,7}, but not in en_US/{\"id\": \1, \"translation\": \1},/g"`
+
+        # printf "\033[0;33m>>>  $JSON <<<\033[0m\n"
         OUTPUT=""
-        DEL_JSON_OUT=`printf "%s\n    %s" "${DEL_JSON_OUT}" "${JSON}"`
+        # DEL_JSON_OUT=`printf "%s\n    %s" "${DEL_JSON_OUT}" "${JSON}"`
         JSON=""
         # printf "\033[0;31m>>> cd plugin; ../bin/i18n4go -c checkup -q i18n -v <<<\033[0m\n"
         # printf "\033[0;31m>>> The translation files are out of sync. Run \`python bin/sync_enUS.py\`.<<<\033[0m\n"
@@ -103,7 +113,7 @@ then
     # JSON panics when it hits tab characters.
     ADD_JSON_OUT=`echo "$ADD_JSON_OUT" | sed -E 's/\t/\\\t/g'`
     printf "\033[0;34m ====== ADD THESE ======= \033[0m \n"
-    echo "$ADD_JSON_OUT"
+    # echo "$ADD_JSON_OUT"
     echo "$ADD_JSON_OUT" > ../old-i18n/add_these.json
 
     DEL_JSON_OUT=${DEL_JSON_OUT%,}
@@ -111,7 +121,7 @@ then
     # JSON panics when it hits tab characters.
     DEL_JSON_OUT=`echo "$DEL_JSON_OUT" | sed -E 's/\t/\\\t/g'`
     printf "\033[0;34m ====== DEL THESE ======= \033[0m \n"
-    echo "$DEL_JSON_OUT"
+    # echo "$DEL_JSON_OUT"
     echo "$DEL_JSON_OUT" > ../old-i18n/remove_these.json
 
 
