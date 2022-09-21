f266fd050e4350465b526cd37e9807b8247acbd6
edsonarios <edsonrios9@gmail.com>
2022-09-21 09:02:09 -0400

    Refactored top level command object-storage: accounts, credential create, credential delete, credential limit, credential list, endpoints commands

--- plugin/commands/objectstorage/accounts_test.go
+++ plugin/commands/objectstorage/accounts_test.go
@@ -6,38 +6,30 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/softlayer/softlayer-go/session"
 
-	"github.com/urfave/cli"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/objectstorage"
-	"github.ibm.com/SoftLayer/softlayer-cli/plugin/managers"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
 )
 
 var _ = Describe("Object Storage list Object Storages", func() {
 	var (
-		fakeUI                   *terminal.FakeUI
-		cmd                      *objectstorage.AccountsCommand
-		cliCommand               cli.Command
-		fakeSession              *session.Session
-		fakeObjectStorageManager managers.ObjectStorageManager
+		fakeUI      *terminal.FakeUI
+		cliCommand  *objectstorage.AccountsCommand
+		fakeSession *session.Session
+		slCommand   *metadata.SoftlayerCommand
 	)
 	BeforeEach(func() {
-		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
-		fakeObjectStorageManager = managers.NewObjectStorageManager(fakeSession)
 		fakeUI = terminal.NewFakeUI()
-		cmd = objectstorage.NewAccountsCommand(fakeUI, fakeObjectStorageManager)
-		cliCommand = cli.Command{
-			Name:        objectstorage.AccountsMetaData().Name,
-			Description: objectstorage.AccountsMetaData().Description,
-			Usage:       objectstorage.AccountsMetaData().Usage,
-			Flags:       objectstorage.AccountsMetaData().Flags,
-			Action:      cmd.Run,
-		}
+		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
+		slCommand = metadata.NewSoftlayerCommand(fakeUI, fakeSession)
+		cliCommand = objectstorage.NewAccountsCommand(slCommand)
+		cliCommand.Command.PersistentFlags().Var(cliCommand.OutputFlag, "output", "--output=JSON for json output.")
 	})
 
 	Describe("Object Storage accounts", func() {
 		Context("Object Storage accounts, Invalid Usage", func() {
 			It("Set command with an invalid output option", func() {
-				err := testhelpers.RunCommand(cliCommand, "--output=xml")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "--output=xml")
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: Invalid output format, only JSON is supported now."))
 			})
@@ -45,13 +37,13 @@ var _ = Describe("Object Storage list Object Storages", func() {
 
 		Context("Object Storage accounts, correct use", func() {
 			It("return objectstorage accounts", func() {
-				err := testhelpers.RunCommand(cliCommand)
+				err := testhelpers.RunCobraCommand(cliCommand.Command)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(fakeUI.Outputs()).To(ContainSubstring("Id          Name     ApiType"))
 				Expect(fakeUI.Outputs()).To(ContainSubstring("123456789   SLUSER   S3"))
 			})
 			It("return objectstorage accounts in format json", func() {
-				err := testhelpers.RunCommand(cliCommand, "--output", "json")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "--output", "json")
 				Expect(err).NotTo(HaveOccurred())
 				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Id": "123456789",`))
 				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Name": "SLUSER",`))
--- plugin/commands/objectstorage/credentiallist_test.go
+++ plugin/commands/objectstorage/credentiallist_test.go
@@ -6,48 +6,40 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/softlayer/softlayer-go/session"
 
-	"github.com/urfave/cli"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/objectstorage"
-	"github.ibm.com/SoftLayer/softlayer-cli/plugin/managers"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
 )
 
 var _ = Describe("Object Storage list Object Storages", func() {
 	var (
-		fakeUI                   *terminal.FakeUI
-		cmd                      *objectstorage.CredentialListCommand
-		cliCommand               cli.Command
-		fakeSession              *session.Session
-		fakeObjectStorageManager managers.ObjectStorageManager
+		fakeUI      *terminal.FakeUI
+		cliCommand  *objectstorage.CredentialListCommand
+		fakeSession *session.Session
+		slCommand   *metadata.SoftlayerCommand
 	)
 	BeforeEach(func() {
-		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
-		fakeObjectStorageManager = managers.NewObjectStorageManager(fakeSession)
 		fakeUI = terminal.NewFakeUI()
-		cmd = objectstorage.NewCredentialListCommand(fakeUI, fakeObjectStorageManager)
-		cliCommand = cli.Command{
-			Name:        objectstorage.CredentialListMetaData().Name,
-			Description: objectstorage.CredentialListMetaData().Description,
-			Usage:       objectstorage.CredentialListMetaData().Usage,
-			Flags:       objectstorage.CredentialListMetaData().Flags,
-			Action:      cmd.Run,
-		}
+		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
+		slCommand = metadata.NewSoftlayerCommand(fakeUI, fakeSession)
+		cliCommand = objectstorage.NewCredentialListCommand(slCommand)
+		cliCommand.Command.PersistentFlags().Var(cliCommand.OutputFlag, "output", "--output=JSON for json output.")
 	})
 
 	Describe("Object Storage endpoints", func() {
 		Context("Object Storage endpoints, Invalid Usage", func() {
 			It("Set command without id", func() {
-				err := testhelpers.RunCommand(cliCommand)
+				err := testhelpers.RunCobraCommand(cliCommand.Command)
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("This command requires one argument"))
 			})
 			It("Set command with id like letters", func() {
-				err := testhelpers.RunCommand(cliCommand, "abc")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "abc")
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Invalid input for 'Storage ID'. It must be a positive integer."))
 			})
 			It("Set command with an invalid output option", func() {
-				err := testhelpers.RunCommand(cliCommand, "123", "--output=xml")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "123", "--output=xml")
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("Incorrect Usage: Invalid output format, only JSON is supported now."))
 			})
@@ -55,15 +47,15 @@ var _ = Describe("Object Storage list Object Storages", func() {
 
 		Context("Object Storage endpoints, correct use", func() {
 			It("return objectstorage endpoints", func() {
-				err := testhelpers.RunCommand(cliCommand, "123")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "123")
 				Expect(err).NotTo(HaveOccurred())
 				Expect(fakeUI.Outputs()).To(ContainSubstring("Id         Password                       Username                 Type Name"))
 				Expect(fakeUI.Outputs()).To(ContainSubstring("17987654   abcdefghijklmnopqrstuvwxyz     123456mnopqrstuvwxyz     S3 Compatible Signature"))
 				Expect(fakeUI.Outputs()).To(ContainSubstring("19987654   aaabcdefghijklmnopqrstuvwxyz   11123456mnopqrstuvwxyz   S3 Compatible Signature"))
-				
+
 			})
 			It("return objectstorage endpoints in format json", func() {
-				err := testhelpers.RunCommand(cliCommand, "123", "--output", "json")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "123", "--output", "json")
 				Expect(err).NotTo(HaveOccurred())
 				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Id": "17987654",`))
 				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Password": "abcdefghijklmnopqrstuvwxyz",`))
