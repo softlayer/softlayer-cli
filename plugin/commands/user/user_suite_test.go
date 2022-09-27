1882bcc614dc26ccf16c531a6906b889c355d5eb
edsonarios <edsonrios9@gmail.com>
2022-09-27 10:31:10 -0400

    Added unit test for commands: reports, security, securitygroup, subnet, tags, ticket, user, virtual, vlan

--- plugin/commands/user/user_suite_test.go
+++ plugin/commands/user/user_suite_test.go
@@ -10,11 +10,27 @@ import (
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/user"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/utils"
 )
 
 func TestUser(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "User Suite")
+}
+
+var availableCommands = []string{
+	"create",
+	"delete",
+	"detail",
+	"detail-edit",
+	"device-access",
+	"edit-notifications",
+	"grant-access",
+	"list",
+	"notifications",
+	"permission-edit",
+	"permissions",
+	"remove-access",
 }
 
 // This test suite exists to make sure commands don't get accidently removed from the SetupCobraCommands
@@ -24,8 +40,30 @@ var _ = Describe("Test user commands", func() {
 	slMeta := metadata.NewSoftlayerCommand(fakeUI, fakeSession)
 
 	Context("New commands testable", func() {
-		autoscaleCommands := user.SetupCobraCommands(slMeta)
-		Expect(autoscaleCommands.Name()).To(Equal("user"))
+		commands := user.SetupCobraCommands(slMeta)
+
+		var arrayCommands = []string{}
+		for _, command := range commands.Commands() {
+			commandName := command.Name()
+			arrayCommands = append(arrayCommands, commandName)
+			It("available commands "+commands.Name(), func() {
+				available := false
+				if utils.StringInSlice(commandName, availableCommands) != -1 {
+					available = true
+				}
+				Expect(available).To(BeTrue(), commandName+" not found in array available Commands")
+			})
+		}
+		for _, command := range availableCommands {
+			commandName := command
+			It("ibmcloud sl "+commands.Name(), func() {
+				available := false
+				if utils.StringInSlice(commandName, arrayCommands) != -1 {
+					available = true
+				}
+				Expect(available).To(BeTrue(), commandName+" not found in ibmcloud sl "+commands.Name())
+			})
+		}
 	})
 
 	Context("User Namespace", func() {
