72aea4e77ff04f796e08a9b94b27fef21fb9470b
edsonarios <edsonrios9@gmail.com>
2022-11-15 10:02:22 -0400

    Added unit test for command autoscale - detail 96.7%

--- plugin/commands/autoscale/detail_test.go
+++ plugin/commands/autoscale/detail_test.go
@@ -1,14 +1,13 @@
 package autoscale_test
 
 import (
-	"errors"
+	"strings"
 
 	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/softlayer/softlayer-go/session"
 
-	"github.com/softlayer/softlayer-go/datatypes"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/autoscale"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
 	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
@@ -16,20 +15,19 @@ import (
 
 var _ = Describe("autoscale detail", func() {
 	var (
-		fakeUI               *terminal.FakeUI
-		cliCommand           *autoscale.DetailCommand
-		fakeSession          *session.Session
-		slCommand            *metadata.SoftlayerCommand
-		fakeAutoScaleManager *testhelpers.FakeAutoScaleManager
+		fakeUI      *terminal.FakeUI
+		cliCommand  *autoscale.DetailCommand
+		fakeSession *session.Session
+		slCommand   *metadata.SoftlayerCommand
 	)
 	BeforeEach(func() {
 		fakeUI = terminal.NewFakeUI()
 		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
-		fakeAutoScaleManager = new(testhelpers.FakeAutoScaleManager)
 		slCommand = metadata.NewSoftlayerCommand(fakeUI, fakeSession)
 		cliCommand = autoscale.NewDetailCommand(slCommand)
 		cliCommand.Command.PersistentFlags().Var(cliCommand.OutputFlag, "output", "--output=JSON for json output.")
-		cliCommand.AutoScaleManager = fakeAutoScaleManager
+		// fakeAutoScaleManager = new(testhelpers.FakeAutoScaleManager)
+		// cliCommand.AutoScaleManager = fakeAutoScaleManager
 	})
 
 	Describe("autoscale detail", func() {
@@ -54,14 +52,46 @@ var _ = Describe("autoscale detail", func() {
 			})
 		})
 
-		Context("Return error", func() {
-			BeforeEach(func() {
-				fakeAutoScaleManager.GetScaleGroupReturns(datatypes.Scale_Group{}, errors.New("Failed to get scale group."))
-			})
-			It("Failed get scale group", func() {
+		Context("Autoscale detail, correct use", func() {
+			It("return autoscale detail", func() {
 				err := testhelpers.RunCobraCommand(cliCommand.Command, "123456")
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(ContainSubstring("Failed to get scale group."))
+				Expect(err).NotTo(HaveOccurred())
+				output := strings.Split(fakeUI.Outputs(), "\n")
+				Expect(output[0]).To(ContainSubstring("Name"))
+				Expect(output[0]).To(ContainSubstring("Value"))
+				Expect(output[1]).To(ContainSubstring("ID"))
+				Expect(output[1]).To(ContainSubstring("12222222"))
+				Expect(output[7]).To(ContainSubstring("Cooldown"))
+				Expect(output[7]).To(ContainSubstring("1800 seconds"))
+				Expect(output[8]).To(ContainSubstring("Last Action"))
+				Expect(output[8]).To(ContainSubstring("2019-10-02T20:26:17Z"))
+				Expect(output[12]).To(ContainSubstring("Virtual Guest Member Template"))
+				Expect(output[12]).To(ContainSubstring("Name"))
+				Expect(output[12]).To(ContainSubstring("Value"))
+				Expect(output[13]).To(ContainSubstring("Hostname"))
+				Expect(output[13]).To(ContainSubstring("testing"))
+				Expect(output[14]).To(ContainSubstring("Domain"))
+				Expect(output[14]).To(ContainSubstring("tech-support.com"))
+			})
+			It("return autoscale detail in format json", func() {
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "123456", "--output", "json")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Name": "ID",`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "12222222"`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Name": "Minimum Members",`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "2"`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Name": "Maximum Members",`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "6"`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "SSH Key 490279   label   "`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "SAN Disk 0       25   "`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "SAN Disk 2       10   "`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "Post Install     https://test.com/   "`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Name": "Active Guests",`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`"Value": "Id   Hostname   Created   "`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`[`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`{`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`}`))
+				Expect(fakeUI.Outputs()).To(ContainSubstring(`]`))
 			})
 		})
 	})
