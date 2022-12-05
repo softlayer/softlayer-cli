74beea56072f4101520b5346bb068958cecb12e3
edsonarios <edsonrios9@gmail.com>
2022-12-05 15:52:41 -0400

    Added suport to windows SO in script bin/generate-i18n-resources.sh

--- bin/generate-i18n-resources.sh
+++ bin/generate-i18n-resources.sh
@@ -3,5 +3,10 @@
 set -e
 
 echo "Generating i18n resource file ..."
-./bin/go-bindata -pkg resources -o plugin/resources/i18n_resources.go plugin/i18n/resources
+if [[ "$OSTYPE" == "msys"* ]]; then
+    # For SO windows
+    ./bin/go-bindata.exe -pkg resources -o plugin/resources/i18n_resources.go plugin/i18n/resources
+else
+    ./bin/go-bindata -pkg resources -o plugin/resources/i18n_resources.go plugin/i18n/resources
+fi
 echo "Done."
