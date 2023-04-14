5f4ed07bab138c6685095fc11905aa566bc333b0
edsonarios <edsonrios9@gmail.com>
2023-04-13 22:24:19 -0400

    Added new command - ibmcloud sl object-storage delete

---
+++ plugin/commands/objectstorage/delete.go
@@ -0,0 +1,81 @@
+package objectstorage
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	slErr "github.ibm.com/SoftLayer/softlayer-cli/plugin/errors"
+	. "github.ibm.com/SoftLayer/softlayer-cli/plugin/i18n"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/managers"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
+)
+
+type DeleteCommand struct {
+	*metadata.SoftlayerCommand
+	Command        *cobra.Command
+	StorageManager managers.StorageManager
+	Reason         string
+	Immediate      bool
+	Force          bool
+}
+
+func NewDeleteCommand(sl *metadata.SoftlayerCommand) *DeleteCommand {
+	thisCmd := &DeleteCommand{
+		SoftlayerCommand: sl,
+		StorageManager:          managers.NewStorageManager(sl.Session),
+	}
+	cobraCmd := &cobra.Command{
+		Use:   "delete " + T("IDENTIFIER"),
+		Short: T("Cancel an existing block storage volume"),
+		Long: T(`${COMMAND_NAME} sl object-storage delete VOLUME_ID [OPTIONS]
+
+EXAMPLE:
+   ${COMMAND_NAME} sl object-storage delete 12345678 --immediate -f 
+   This command cancels volume with ID 12345678 immediately and without asking for confirmation.`),
+		Args: metadata.OneArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return thisCmd.Run(args)
+		},
+	}
+	cobraCmd.Flags().StringVar(&thisCmd.Reason, "reason", "", T("An optional reason for cancellation"))
+	cobraCmd.Flags().BoolVar(&thisCmd.Immediate, "immediate", false, T("Cancel the block storage volume immediately instead of on the billing anniversary"))
+	cobraCmd.Flags().BoolVarP(&thisCmd.Force, "force", "f", false, T("Force operation without confirmation"))
+	thisCmd.Command = cobraCmd
+	return thisCmd
+}
+
+func (cmd *DeleteCommand) Run(args []string) error {
+
+	objectStorageID, err := strconv.Atoi(args[0])
+	subs := map[string]interface{}{"ID": objectStorageID, "objectStorageID": objectStorageID}
+	if err != nil {
+		return slErr.NewInvalidSoftlayerIdInputError("objectStorageID")
+	}
+	if !cmd.Force {
+		confirm, err := cmd.UI.Confirm(T("This action will incur charges on your account. Continue?", subs))
+		if err != nil {
+			return err
+		}
+		if !confirm {
+			cmd.UI.Print(T("Aborted."))
+			return nil
+		}
+	}
+
+	err = cmd.StorageManager.CancelVolume("block", objectStorageID, cmd.Reason, cmd.Immediate)
+	if err != nil {
+		if strings.Contains(err.Error(), slErr.SL_EXP_OBJ_NOT_FOUND) {
+			return slErr.NewAPIError(T("Unable to find object-storage with objectStorageID {{.objectStorageID}}.\n", subs), err.Error(), 0)
+		}
+		return slErr.NewAPIError(T("Failed to cancel object-storage: {{.objectStorageID}}.\n", subs), err.Error(), 2)
+	}
+	cmd.UI.Ok()
+	if cmd.Immediate {
+		cmd.UI.Print(T("Object-storage {{.objectStorageID}} has been marked for immediate cancellation.", subs))
+	} else {
+		cmd.UI.Print(T("Object-storage {{.objectStorageID}} has been marked for cancellation.", subs))
+	}
+	return nil
+}
---
+++ plugin/commands/objectstorage/delete_test.go
@@ -0,0 +1,64 @@
+package objectstorage_test
+
+import (
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/testhelpers/terminal"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/softlayer/softlayer-go/session"
+
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/commands/objectstorage"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/metadata"
+	"github.ibm.com/SoftLayer/softlayer-cli/plugin/testhelpers"
+)
+
+var _ = Describe("Object Storage delete", func() {
+	var (
+		fakeUI      *terminal.FakeUI
+		cliCommand  *objectstorage.DeleteCommand
+		fakeSession *session.Session
+		slCommand   *metadata.SoftlayerCommand
+	)
+	BeforeEach(func() {
+		fakeUI = terminal.NewFakeUI()
+		fakeSession = testhelpers.NewFakeSoftlayerSession([]string{})
+		slCommand = metadata.NewSoftlayerCommand(fakeUI, fakeSession)
+		cliCommand = objectstorage.NewDeleteCommand(slCommand)
+		cliCommand.Command.PersistentFlags().Var(cliCommand.OutputFlag, "output", "--output=JSON for json output.")
+	})
+
+	Describe("Object Storage delete", func() {
+		Context("Object Storage delete, Invalid Usage", func() {
+			It("Set command without id", func() {
+				err := testhelpers.RunCobraCommand(cliCommand.Command)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("This command requires one argument"))
+			})
+			It("Set command with id like letters", func() {
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "abc")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Invalid input for 'objectStorageID'. It must be a positive integer."))
+			})
+			It("Set command without confirmation", func() {
+				fakeUI.Inputs("No")
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "123456")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring("This action will incur charges on your account. Continue?"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Aborted."))
+			})
+		})
+		Context("Object Storage delete , correct use", func() {
+			It("return objectstorage delete", func() {
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "123456", "--force")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Object-storage"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("has been marked for cancellation."))
+			})
+			It("return objectstorage delete with inmediate option", func() {
+				err := testhelpers.RunCobraCommand(cliCommand.Command, "123456", "--force", "--immediate", "--reason", "cancel test")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(fakeUI.Outputs()).To(ContainSubstring("Object-storage"))
+				Expect(fakeUI.Outputs()).To(ContainSubstring("has been marked for immediate cancellation."))
+			})
+		})
+	})
+})
