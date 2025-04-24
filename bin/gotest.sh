bebbec1daa04358d898a62e778a0ec9688b8fbe5
Christopher Gallo <cgallo@us.ibm.com>
2025-04-24 17:40:40 -0500

    Trying to get the build to run properly

---
+++ bin/gotest.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+
+go vet $(go list ./... | grep -v "fixtures" | grep -v "vendor")
+go test $(go list ./... | grep -v "fixtures" | grep -v "vendor")
+gosec -exclude-dir=fixture -exclude-dir=plugin/resources -quiet ./...
+go build
